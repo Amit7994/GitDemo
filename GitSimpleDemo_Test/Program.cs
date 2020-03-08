81a4ba3f62ca8638c0026eabb87b0c1ee2cbf34a
Amit7994 <58580899+Amit7994@users.noreply.github.com>
2020-03-08 17:43:27 +0530

    Add project files.

---
+++ GitSimpleDemo_Test/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitSimpleDemo_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World..!!");
+            Console.WriteLine("Changes Made By Amit...");
+            Console.ReadLine();
+        }
+    }
+}
